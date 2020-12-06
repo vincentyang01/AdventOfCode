e14392737104de4cccc90242cb5ccf8fe40436d0
vincentyang01 <vincentyangcsi@gmail.com>
2020-12-06 00:53:59 -0500

    day6 - part 2

--- day6.js
+++ day6.js
@@ -17,3 +17,29 @@ function day6(){
     })
 }
 console.log(day6())
+
+function day6part2(){
+    let fs = require('fs') 
+    fs.readFile('./day6.txt', 'utf8', function (err, data) {
+        if (err) throw err;
+        let groupForms = data.split('\n\n')
+        let count = 0
+        for(let i = 0; i < groupForms.length; i++){
+            let individualForms = groupForms[i].split('\n')
+            let hash = {}
+            for(let j = 0; j < groupForms[i].length; j++){
+                if(!(groupForms[i][j] in hash) && (groupForms[i][j] !== '\n')){
+                    hash[groupForms[i][j]] = 1
+                } else {
+                    hash[groupForms[i][j]]++
+                }
+            }
+            let arr = Object.values(hash)
+            for(let k = 0; k < arr.length; k++){
+                arr[k] === individualForms.length ? count++ : null
+            }
+        }
+        console.log(count)
+    })
+}
+console.log(day6part2())
