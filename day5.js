c23d6c6bd9144b60f7abb7b5b7e051115bc42a09
vincentyang01 <vincentyangcsi@gmail.com>
2020-12-05 20:59:57 -0500

    day5 - part 2

--- day5.js
+++ day5.js
@@ -1,49 +1,61 @@
+// let directions = "FBFBBFFRLR" //357
+// let directions = "BFFFBBFRRR" //567
+// let directions = "FFFBBBFRRR" //119
+// let directions = "BBFFBBFRLL"
 function day5(){
-    let highest = 0
+    let arr = []
+    let max = 0;
+    let row = 0;
+    let column = 0;
+    let id = 0
     let fs = require('fs') 
     fs.readFile('./day5.txt', 'utf8', function (err, data) {
         if (err) throw err;
         let directions = data.split('\n')
-    // let directions = "FBFBBFFRLR" //357
-    // let directions = "BFFFBBFRRR" //567
-    // let directions = "FFFBBBFRRR" //119
         for(let i = 0; i < directions.length; i++){
-            console.log(directions[i])
             let front = 0;
             let back = 127;
-            let mid = 63
-            for(let j = 0; j < 7; j++){
-                if(directions[i][j] === 'B'){
-                    front = mid
-                    mid = Math.floor((front + back) / 2)
-                    console.log("F = ", front, "M = ", mid, "B = ", back)
-                } else if(directions[i][j] === 'F') {
-                    back = mid
-                    mid = Math.ceil((front + back) / 2)
-                    console.log("F = ", front, "M = ", mid, "B = ", back)
-                }
-            }
-            // console.log("Answer is: ", mid)
             let left = 0
             let right = 7
-            let center = 4
-            for(let j = 7; j < 10; j++){
-                if(directions[i][j] === 'L'){
-                    right = center
-                    center = Math.floor((left + right) / 2)
-                    console.log("L = ", left,"C = ", center, "R = ", right)
-                } else if(directions[i][j] === 'R'){
-                    left = center
-                    center = Math.ceil((left + right) / 2)
-                    console.log("L = ", left,"C = ", center, "R = ", right)
+            for(let j = 0; j < 7; j++){
+                if(directions[i][j] === 'F') {
+                    back = Math.floor((back + front) / 2)
+                    console.log("F = ", front, "B = ", back)
+                } 
+                if(directions[i][j] === 'B') {
+                    front = Math.ceil((back + front) / 2)
+                    console.log("F = ", front, "B = ", back)
                 }
             }
-            console.log(mid, center)
-            console.log(mid * 8 + center)
-            mid * 8 + center > highest ? highest = mid * 8 + center : null
-            console.log(highest)
+            for(let j = 7; j < 10; j++){
+                if(directions[i][j] === 'L'){
+                    right = Math.floor((right + left) / 2)
+                    console.log("L = ", left, "R = ", right)
+                } 
+                if(directions[i][j] === 'R'){
+                    left = Math.ceil((left + right) / 2)
+                    console.log("L = ", left, "R = ", right)
+                }
+            }
+            directions[6] === "F" ? row = front : row = back
+            directions[9] === "R" ? column = right : column = left
+            id = row * 8 + column
+            console.log(id)
+            arr.push(id)
+        }
+        
+        arr.sort(function (a, b) {
+            return a-b;
+        });
+        console.log(arr)
+        console.log(Math.max(...arr))
+
+        for(let k = 0; k < arr.length - 1; k++){
+            // console.log(arr[k])
+            if (arr[k] - arr[k-1] !== 1) {
+                console.log("missing seat is " + (parseInt(arr[k] - 1)));
+            }        
         }
     })
 }
-
 console.log(day5())
